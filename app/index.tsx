002b0590f32fd5a306e083e3f171eaf0303208ed
Daniel Velez <daniel@reggora.com>
2023-11-17 14:49:34 -0500

    added the apollo client library and the provider

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const StarshipTracker = () => {
+  return (
+    <SafeAreaView>
+      <Text>We are ready to track some spaceships!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default StarshipTracker;
