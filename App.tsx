dce8e8c730ea1bbdc2f28464abebd68dfc1a9f1f
Daniel Velez <daniel@reggora.com>
2023-11-17 15:26:15 -0500

    Display all the Starships in a FlatList

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {useColorScheme} from 'react-native';
 import {ApolloProvider} from '@apollo/client';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -27,24 +26,5 @@ function App(): JSX.Element {
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, FlatList} from 'react-native';
+import {useQuery, gql} from '@apollo/client';
+
+const GET_STARSHIPS = gql`
+  query GetStarships {
+    allStarships {
+      starships {
+        name
+        model
+        starshipClass
+        manufacturers
+        costInCredits
+        length
+        crew
+        passengers
+        maxAtmospheringSpeed
+        hyperdriveRating
+        MGLT
+        cargoCapacity
+        consumables
+        pilotConnection {
+          pilots {
+            name
+          }
+        }
+        filmConnection {
+          films {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
 
 const StarshipTracker = () => {
+  const {loading, error, data} = useQuery(GET_STARSHIPS);
+
+  console.log(JSON.stringify(data, null, 2));
+
   return (
     <SafeAreaView>
       <Text>We are ready to track some spaceships!</Text>
+      <FlatList
+        data={data?.allStarships?.starships}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+        keyExtractor={item => item.name}
+      />
     </SafeAreaView>
   );
 };
